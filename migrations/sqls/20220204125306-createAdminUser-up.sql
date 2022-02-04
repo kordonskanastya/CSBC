0405c4b65f19be26982cffab8e15eff5f1220410
kordonskanastya <kordonskanastya@gmail.com>
2022-02-04 15:03:36 +0200

    make migration for create user

---
+++ migrations/sqls/20220204125306-createAdminUser-up.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username,surname,patronymic,email,"password",role_id,refreshtoken) VALUES
+ ('Vova_4','Batkovich_4','zhopa_4','kordonskanastya@gmail.com','56f15335902f5a9e1c8be26e8e6f8bcec630848d84bea5b3a998a1665e0edaaa',NULL,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtvcmRvbnNrYW5hc3R5YUBnbWFpbC5jb20iLCJpYXQiOjE2NDM3NDExMzJ9.1Yp7WoMjiwLh7uo4Skv4em8ZzXHJMsYtwsk5rh_sKt0');
