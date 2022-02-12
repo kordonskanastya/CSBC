67c8e5d7433759e041d00ea045d2dc654b9fea71
kordonskanastya <kordonskanastya@gmail.com>
2022-02-12 08:26:33 +0200

    fix user migration

--- migrations/sqls/20220204125306-createAdminUser-up.sql
+++ migrations/sqls/20220204125306-createAdminUser-up.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (username,surname,patronymic,email,"password",role,refreshtoken) VALUES
- ('Vova','Vovan','Batkovich','adminadmin@gmail.com','10fe29fd480fa5f2daa8e23d0df3eed5aa13c2e8dcf68dbcd0e420411a3bfb54',NULL,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtvcmRvbnNrYW5hc3R5YUBnbWFpbC5jb20iLCJpYXQiOjE2NDM3NDExMzJ9.1Yp7WoMjiwLh7uo4Skv4em8ZzXHJMsYtwsk5rh_sKt0');
+ ('Vova','Vovan','Batkovich','adminadmin@gmail.com','10fe29fd480fa5f2daa8e23d0df3eed5aa13c2e8dcf68dbcd0e420411a3bfb54','admin','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtvcmRvbnNrYW5hc3R5YUBnbWFpbC5jb20iLCJpYXQiOjE2NDM3NDExMzJ9.1Yp7WoMjiwLh7uo4Skv4em8ZzXHJMsYtwsk5rh_sKt0');
