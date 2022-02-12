771b3c48762cbe3934845e9436260b50bbfd1a7a
kordonskanastya <kordonskanastya@gmail.com>
2022-02-12 08:47:51 +0200

    create migration to fill specialities table

---
+++ migrations/sqls/20220212063530-fillTableSpecialities-up.sql
@@ -0,0 +1,19 @@
+INSERT INTO specialities (name, code) VALUES
+('Економіка', 051);
+INSERT INTO specialities (name, code) VALUES
+('Облік і оподаткування', 071);
+INSERT INTO specialities (name, code) VALUES
+('Фінанси, банківська справа,
+страхування', 072);
+INSERT INTO specialities (name, code) VALUES
+('Маркетинг', 075);
+INSERT INTO specialities (name, code) VALUES
+('Підприємництво, торгівля
+та біржова діяльність', 076);
+INSERT INTO specialities (name, code) VALUES
+('Інженерія програмного
+забезпечення', 121);
+INSERT INTO specialities (name, code) VALUES
+('Комп’ютерна інженерія', 123);
+INSERT INTO specialities (name, code) VALUES
+('Дизайн', 022);
