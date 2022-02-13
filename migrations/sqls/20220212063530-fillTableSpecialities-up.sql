5ad7e4540876c12419d774081b8ad4bab56b0914
kordonskanastya <kordonskanastya@gmail.com>
2022-02-13 17:45:21 +0200

    make changes in migrations

--- migrations/sqls/20220212063530-fillTableSpecialities-up.sql
+++ migrations/sqls/20220212063530-fillTableSpecialities-up.sql
@@ -1,19 +1,16 @@
 INSERT INTO specialities (name, code) VALUES
-('Економіка', 051);
+('Економіка', '051');
 INSERT INTO specialities (name, code) VALUES
-('Облік і оподаткування', 071);
+('Облік і оподаткування', '071');
 INSERT INTO specialities (name, code) VALUES
-('Фінанси, банківська справа,
-страхування', 072);
+('Фінанси, банківська справа, страхування', '072');
 INSERT INTO specialities (name, code) VALUES
-('Маркетинг', 075);
+('Маркетинг', '075');
 INSERT INTO specialities (name, code) VALUES
-('Підприємництво, торгівля
-та біржова діяльність', 076);
+('Підприємництво, торгівля та біржова діяльність', '076');
 INSERT INTO specialities (name, code) VALUES
-('Інженерія програмного
-забезпечення', 121);
+('Інженерія програмного забезпечення', '121');
 INSERT INTO specialities (name, code) VALUES
-('Комп’ютерна інженерія', 123);
+('Комп’ютерна інженерія', '123');
 INSERT INTO specialities (name, code) VALUES
-('Дизайн', 022);
+('Дизайн', '022');
